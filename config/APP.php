fd60b5fc08181e7f57953886a362d3b1b9507fd0
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-07-17 23:52:14 -0400

    Estructura basica del proyecto y configuración de la vista index.php

---
+++ config/APP.php
@@ -0,0 +1,8 @@
+<?php
+    /* Variables Globales */
+    const SERVERURL = "http://localhost/prestamos/";
+    const COMPANY = "SISTEMA PRESTAMOS";
+    const MONEDA = "$";
+    
+    /* https://www.php.net/manual/es/timezones.php */
+    date_default_timezone_set("America/La_Paz");
---
+++ config/SERVER.php
@@ -0,0 +1,16 @@
+<?php
+    /* Parametros de Conexión con la Base de Datos */
+    const SERVER = "localhost";
+    const DB = "prestamos";
+    const USER = "root";
+    const PASS = "";
+
+    const SGBD = "mysql:host=" . SERVER . ";dbname=" . DB;
+
+    /* Variables de Encriptación */
+    const METHOD = "AES-256-CBC";
+    const SECRET_KEY = '$PRESTAMOS@2021';
+    const SECRET_ID = '601848';
+
+
+
