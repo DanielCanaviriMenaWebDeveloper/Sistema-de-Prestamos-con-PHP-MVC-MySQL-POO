24e2ecb0ff7f22a1c5f79988bce3f9e6a4f50ea7
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-07-19 13:48:11 -0400

    Creando todas las vistas de nuestro proyecto

--- modelos/vistasModelo.php
+++ modelos/vistasModelo.php
@@ -4,7 +4,7 @@
 
         /********** Modelo obtener vistas **********/
         protected static function obtener_vistas_modelo($vistas) {
-            $listaBlanca = [];
+            $listaBlanca = ["home", "company", "client-list", "client-new", "client-search", "client-update", "item-list", "item-new", "item-search", "item-update", "reservation-new", "reservation-list", "reservation-pending", "reservation-reservation", "reservation-search", "reservation-update", "user-list", "user-new", "user-search", "user-update"];
             if (in_array($vistas, $listaBlanca)) { // Metodo que verifica si existe un string determinado dentro un array.
                 if (is_file("./vistas/contenidos/" . $vistas . "-view.php")) {
                     $contenido = "./vistas/contenidos/" . $vistas . "-view.php";
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -10,6 +10,16 @@
     </head>
 
     <body>
+        <?php 
+            $peticionAjax = false;
+            require_once "./controladores/vistasControlador.php";
+            /* $IV : Instancia a las vistas */
+            $IV = new vistasControlador();
+            $vistas = $IV->obtener_vistas_controlador();
+            if ($vistas == "login" || $vistas == "404") {
+                require_once "./vistas/contenidos/" . $vistas . "-view.php";
+            } else {
+        ?>
 
         <!-- Main container -->
         <main class="full-box main-container">
@@ -18,14 +28,20 @@
 
             <!-- Page content -->
             <section class="full-box page-content">
-                <?php include "./vistas/inc/NavBar.php"; ?>
+                <?php 
+                    include "./vistas/inc/NavBar.php"; 
+                    include $vistas;
+                ?>
+
             </section>
         </main>
-
 
         <!--=============================================
         =            Include JavaScript files           =
         ==============================================-->
-        <?php include "./vistas/inc/Script.php"; ?>
+        <?php
+            }/* Close else */
+            include "./vistas/inc/Script.php"; 
+        ?>
     </body>
 </html>
