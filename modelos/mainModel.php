d2d2c4309807cdaef55c3e5b1f547183c4e0eb7f
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-07-26 22:46:27 -0400

    Función para limpiar cadenas de texto

--- modelos/mainModel.php
+++ modelos/mainModel.php
@@ -42,12 +42,45 @@
         }
 
         /********** Función para generar códigos aleatorios **********/
-        /* La sintaxis que tomaran los códigos generados sera como el siguiente ejemplo: D12345-1 */
+        // La sintaxis que tomaran los códigos generados sera como el siguiente ejemplo: D12345-1 
         protected static function generar_codigo_aleatorio($letra, $longitud, $numero) {
             for($i = 1; $i <= $longitud; $i++) {
                 $aleatorio = rand(0, 9);
-                $letra .=  $aleatorio; /* Concatena los números generados aleatoriamente a la variable $letra. */
+                $letra .=  $aleatorio; // Concatena los números generados aleatoriamente a la variable $letra. 
             }
             return $letra . "-" . $numero;
+        }
+
+        /********** Función para limpiar cadenas **********/
+        protected static function limpiar_cadena($cadena) {
+            $cadena = trim($cadena); // Elimina espacios antes o despues de la cadena. 
+            $cadena = stripslashes($cadena); // Elimina las barras invertidas dentro la cadena : "\" 
+            $cadena = str_ireplace("<script>", "", $cadena); // Remplaza en la cadena la subcadena "<script>" por "", en otras palabra elimina la subcadena.
+            $cadena = str_ireplace("</script>", "", $cadena);
+            $cadena = str_ireplace("<script src=>", "", $cadena);
+            $cadena = str_ireplace("<script type=>", "", $cadena);
+            $cadena = str_ireplace("SELECT * FROM", "", $cadena);
+            $cadena = str_ireplace("DELETE FROM", "", $cadena);
+            $cadena = str_ireplace("INSERT INTO", "", $cadena);
+            $cadena = str_ireplace("DROP TABLE", "", $cadena);
+            $cadena = str_ireplace("DROP DATABASE", "", $cadena);
+            $cadena = str_ireplace("TRUNCATE TABLE", "", $cadena);
+            $cadena = str_ireplace("SHOW TABLES", "", $cadena);
+            $cadena = str_ireplace("SHOW DATABASES", "", $cadena);
+            $cadena = str_ireplace("<?php", "", $cadena);
+            $cadena = str_ireplace("?>", "", $cadena);
+            $cadena = str_ireplace("--", "", $cadena);
+            $cadena = str_ireplace(">", "", $cadena);
+            $cadena = str_ireplace("<", "", $cadena);
+            $cadena = str_ireplace("[", "", $cadena);
+            $cadena = str_ireplace("]", "", $cadena);
+            $cadena = str_ireplace("^", "", $cadena);
+            $cadena = str_ireplace("==", "", $cadena);
+            $cadena = str_ireplace(";", "", $cadena);
+            $cadena = str_ireplace("::", "", $cadena);
+            $cadena = stripslashes("$cadena", "", $cadena);
+            $cadena = stripslashes($cadena);
+            $cadena = trim($cadena);
+            return $cadena;
         }
     }
