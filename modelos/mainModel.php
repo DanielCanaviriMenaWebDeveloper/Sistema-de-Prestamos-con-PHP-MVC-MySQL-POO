a13697a177ce6a8551b42fea8e77b7e6383fc121
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-07-28 00:18:21 -0400

    Función paginador de tablas parte 1

--- modelos/mainModel.php
+++ modelos/mainModel.php
@@ -78,7 +78,6 @@
             $cadena = str_ireplace("==", "", $cadena);
             $cadena = str_ireplace(";", "", $cadena);
             $cadena = str_ireplace("::", "", $cadena);
-            $cadena = stripslashes("$cadena", "", $cadena);
             $cadena = stripslashes($cadena);
             $cadena = trim($cadena);
             return $cadena;
@@ -96,12 +95,40 @@
 
         /********** Función verificar fechas **********/
         protected static function verificar_fecha($fecha) {
+            $valores = explode("-", $fecha); // Divide un string en varios strings, lo devuelve en un array de strings.
             // https://www.php.net/manual/es/function.checkdate.php
-            $valores = explode("-", $fecha);
             if (count($valores) == 3 && checkdate($valores[1], $valores[2], $valores[0] )) {
                 return false; // No tiene errores la fecha, si coincide con el formato solicitado
             }else {
                 return true; // Si tiene errores la fecha, no coincide con el formato solicitado
             }
         }
+
+        /********** Función paginador de tablas **********/
+        protected static function paginador_tablas($pagina, $Npaginas, $url, $botones) {
+            $tabla = '<nav aria-label="Page navigation example"><ul class="pagination justify-content-center">';
+
+            if ($pagina == 1) {
+                $tabla .= ' <li class="page-item disabled"><a class="page-link"><i class="fas fa-angle-double-left"></i></a></li>';
+            }else {
+                $tabla .= '
+                    <li class="page-item"><a class="page-link" href=" '. $url .'1/ "><i class="fas fa-angle-double-left"></i></a></li>
+                    <li class="page-item"><a class="page-link" href=" '. $url . ($pagina - 1) .'/ ">Anterior</a></li>
+                ';
+            }
+
+            if ($pagina == $Npaginas) {
+                $tabla .= ' <li class="page-item disabled"><a class="page-link"><i class="fas fa-angle-double-right"></i></a></li>';
+            } else {
+                $tabla .= '
+                    <li class="page-item"><a class="page-link" href=" ' . $url . ($pagina + 1) . '/ ">Siguiente</a></li>
+                    <li class="page-item"><a class="page-link" href=" ' . $url . $Npaginas . '/ "><i class="fas fa-angle-double-right"></i></a></li>
+                ';
+            }
+
+            $tabla .= '</ul></nav>';
+            return $tabla;
+        }
+
+
     }
