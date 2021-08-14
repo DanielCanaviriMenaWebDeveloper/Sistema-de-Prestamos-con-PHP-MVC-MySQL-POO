958b659d35c5333c22c8c4589113f08c7bd87538
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-08-13 23:54:06 -0400

    Función enviar_formulario_ajax(e) finalizado

--- vistas/contenidos/user-new-view.php
+++ vistas/contenidos/user-new-view.php
@@ -24,78 +24,91 @@
 
 <!-- Content -->
 <div class="container-fluid">
-    <form action="" class="form-neon" autocomplete="off">
+    <form class="form-neon FormularioAjax" action="" method="POST" data-form="save" autocomplete="off">
+        <!-- Información Personal -->
         <fieldset>
             <legend><i class="far fa-address-card"></i> &nbsp; Información personal</legend>
             <div class="container-fluid">
                 <div class="row">
+                    <!-- Usuario DNI -->
                     <div class="col-12 col-md-4">
                         <div class="form-group">
                             <label for="usuario_dni" class="bmd-label-floating">DNI</label>
                             <input type="text" pattern="[0-9-]{1,20}" class="form-control" name="usuario_dni_reg" id="usuario_dni" maxlength="20">
                         </div>
                     </div>
-
+                    <!-- Usuario Nombres -->
                     <div class="col-12 col-md-4">
                         <div class="form-group">
                             <label for="usuario_nombre" class="bmd-label-floating">Nombres</label>
                             <input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" name="usuario_nombre_reg" id="usuario_nombre" maxlength="35">
                         </div>
                     </div>
+                    <!-- Usuario Apellidos -->
                     <div class="col-12 col-md-4">
                         <div class="form-group">
                             <label for="usuario_apellido" class="bmd-label-floating">Apellidos</label>
                             <input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" name="usuario_apellido_reg" id="usuario_apellido" maxlength="35">
                         </div>
                     </div>
+                    <!-- Telefono -->
                     <div class="col-12 col-md-6">
                         <div class="form-group">
                             <label for="usuario_telefono" class="bmd-label-floating">Teléfono</label>
                             <input type="text" pattern="[0-9()+]{8,20}" class="form-control" name="usuario_telefono_reg" id="usuario_telefono" maxlength="20">
                         </div>
                     </div>
+                    <!-- Dirección -->
                     <div class="col-12 col-md-6">
                         <div class="form-group">
                             <label for="usuario_direccion" class="bmd-label-floating">Dirección</label>
                             <input type="text" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ().,#\- ]{1,190}" class="form-control" name="usuario_direccion_reg" id="usuario_direccion" maxlength="190">
                         </div>
                     </div>
-                </div>
+                </div><!-- .row -->
             </div>
         </fieldset>
         <br><br><br>
+
+        <!-- Información de la cuenta -->
         <fieldset>
             <legend><i class="fas fa-user-lock"></i> &nbsp; Información de la cuenta</legend>
             <div class="container-fluid">
                 <div class="row">
+                    <!-- Nombre de Usuario -->
                     <div class="col-12 col-md-6">
                         <div class="form-group">
                             <label for="usuario_usuario" class="bmd-label-floating">Nombre de usuario</label>
                             <input type="text" pattern="[a-zA-Z0-9]{1,35}" class="form-control" name="usuario_usuario_reg" id="usuario_usuario" maxlength="35">
                         </div>
                     </div>
+                    <!-- Email -->
                     <div class="col-12 col-md-6">
                         <div class="form-group">
                             <label for="usuario_email" class="bmd-label-floating">Email</label>
                             <input type="email" class="form-control" name="usuario_email_reg" id="usuario_email" maxlength="70">
                         </div>
                     </div>
+                    <!-- Contraseña -->
                     <div class="col-12 col-md-6">
                         <div class="form-group">
                             <label for="usuario_clave_1" class="bmd-label-floating">Contraseña</label>
                             <input type="password" class="form-control" name="usuario_clave_1_reg" id="usuario_clave_1" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required="">
                         </div>
                     </div>
+                    <!-- Repetir Contraseña -->
                     <div class="col-12 col-md-6">
                         <div class="form-group">
                             <label for="usuario_clave_2" class="bmd-label-floating">Repetir contraseña</label>
                             <input type="password" class="form-control" name="usuario_clave_2_reg" id="usuario_clave_2" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required="">
                         </div>
                     </div>
-                </div>
+                </div><!-- .row -->
             </div>
         </fieldset>
         <br><br><br>
+
+        <!-- Nivel de privilegio -->
         <fieldset>
             <legend><i class="fas fa-medal"></i> &nbsp; Nivel de privilegio</legend>
             <div class="container-fluid">
@@ -113,9 +126,10 @@
                             </select>
                         </div>
                     </div>
-                </div>
+                </div><!-- .row -->
             </div>
         </fieldset>
+
         <p class="text-center" style="margin-top: 40px;">
             <button type="reset" class="btn btn-raised btn-secondary btn-sm"><i class="fas fa-paint-roller"></i> &nbsp; LIMPIAR</button>
             &nbsp; &nbsp;
